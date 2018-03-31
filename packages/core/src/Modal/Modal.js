75489461f01801f0ef46a4622b80fc1c335d61a9
Steven Fitzpatrick <steven.fitzpatrick1985@gmail.com>
2018-03-31 22:54:26 +0100

    feat(core) Initial Setup

--- packages/core/src/Modal/Modal.js
+++ packages/core/src/Modal/Modal.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { ModalWrapper, ModalDialog, ModalContent } from './Modal.styled';
+
 export class Modal extends React.Component {
   render() {
     return <dialog />;
